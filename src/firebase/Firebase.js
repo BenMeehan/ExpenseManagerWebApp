aee135523998a08b9e9836222b1b9b4ff00a8f66
ben <benmeehan111@gmail.com>
2021-01-20 12:05:39 +0530

    added env

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBfSREb-nFAi7ca-dJiWFnUXpX74uIVHxo",
-  authDomain: "expensemanager-cb53d.firebaseapp.com",
-  databaseURL: "https://expensemanager-cb53d-default-rtdb.firebaseio.com",
-  projectId: "expensemanager-cb53d",
-  storageBucket: "expensemanager-cb53d.appspot.com",
-  messagingSenderId: "1005637497134",
-  appId: "1:1005637497134:web:c02fa99ea4c5dd657a4abf",
-  measurementId: "G-WEW3385Z14",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);

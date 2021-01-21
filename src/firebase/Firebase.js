6247ed6318f7375064ed331cc7b6f5279ed51b76
ben <benmeehan111@gmail.com>
2021-01-21 17:53:48 +0530

    google authentication

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,10 +1,10 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBfSREb-nFAi7ca-dJiWFnUXpX74uIVHxo",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: "expensemanager-cb53d",
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
